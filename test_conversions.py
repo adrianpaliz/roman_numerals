9aee05bfbb65dabf35128c58cc28def028122530
adrianpaliz <adrianpaliz@gmail.com>
2022-02-09 13:48:44 -0500

    Basic test file with print for functons.

---
+++ test_conversions.py
@@ -0,0 +1,8 @@
+from roman_numerals import arabic_to_roman, roman_to_arabic
+
+print(arabic_to_roman(36))
+print(arabic_to_roman(46))
+
+print(roman_to_arabic('MMCCCXXIV'))
+print(roman_to_arabic('MMMMM'))
+print(roman_to_arabic('IM'))
