2f53514003d7c5951cb25fa59088c1bcad82932d
adrianpaliz <adrianpaliz@gmail.com>
2022-02-12 10:24:31 -0500

    Change of names following the style guide, fix an error on roman_to_arabic function for 'IIII' string

--- roman_numerals.py
+++ roman_numerals.py
@@ -4,6 +4,7 @@ roman_values = {
     5: 'V',
     9: 'IX',
     10: 'X',
+    40: 'XL',
     50: 'L',
     90: 'XC',
     100: 'C',
@@ -13,7 +14,7 @@ roman_values = {
     1000: 'M'
 }
 
-inverted_roman_values = {
+roman_symbols = {
     'I': 1,
     'V': 5,
     'X': 10,
@@ -23,24 +24,26 @@ inverted_roman_values = {
     'M': 1000
     }
 
-class Roman_error(Exception):
+class RomanError(Exception):
     pass
 
 def number_validation(number):
     if not isinstance(number, int):
         raise TypeError(f"{number} mush be a integer")
+
     if number <= 0:
         raise ValueError(f"{number} mush be a positive integer")
 
 def arabic_to_roman(number):
     number_validation(number)  
     roman = ''
-    remainder = 1
+    remainder = None
 
     while remainder != 0:        
         for value in sorted(roman_values.keys(), reverse = True):
             if number >= value:
-                break    
+                break  
+
         quotient = number // value
         remainder = number % value
         roman += quotient * roman_values[value]        
@@ -48,31 +51,52 @@ def arabic_to_roman(number):
     
     return roman
 
+def symbol_to_value(symbol):
+    try:
+        return roman_symbols[symbol]
+    except KeyError as error_key:
+        raise RomanError(f"Syntax error. Symbol {error_key} not allowed")    
+
 def roman_to_arabic(string):
     result = 0
     repetitions_counter = 0
+    string = string.upper()
     
     for index in range(len(string) - 1):
         letter = string[index]
-        next = string[index + 1]
-
+        next_letter = string[index + 1]
+        letter_value = symbol_to_value(letter)
+        next_letter_value = symbol_to_value(next_letter)
+       
         # Repetitions tester
-        if letter == next:
+        if letter == next_letter:
             repetitions_counter += 1
-        elif inverted_roman_values[letter] < inverted_roman_values[next] and repetitions_counter > 0:
-            raise Roman_error(f"Syntax error. {letter} tree repetitions dont could subtract")
+        elif letter_value < next_letter_value and repetitions_counter > 0:
+            raise RomanError(f"Syntax error. {letter} repetitions before subtract")
         else:
             repetitions_counter = 0
-        
-        if letter in 'VLD' and repetitions_counter > 0 or repetitions_counter > 2 :
-            raise Roman_error(f"Syntax error. Too many repetitions of {letter}")
+       
+        print(repetitions_counter)
+        if letter in 'VLD' and repetitions_counter > 0:
+            raise RomanError(f"Syntax error. Too many repetitions of {letter}")        
+        elif repetitions_counter > 2:
+            raise RomanError(f"Syntax error. Too many repetitions of {letter}")
 
-        if inverted_roman_values[letter] >= inverted_roman_values[next]:
-            result += inverted_roman_values[letter]
+        if letter_value >= next_letter_value:
+            #Always sum
+            result += letter_value
         else:
+            # Subtraction tests
             if letter in 'VLD':
-                raise Roman_error(f"Syntax error. {letter} can't subtract")
-            result -= inverted_roman_values[letter]    
+                raise RomanError(f"Syntax error. {letter} can't subtract")
+            elif letter == 'I' and next_letter not in ('XV'):
+                raise RomanError(f"Syntax error. {letter}{next_letter} not allowed")
+            elif letter == 'X' and next_letter not in ('LC'):
+                raise RomanError(f"Syntax error. {letter}{next_letter} not allowed")
+            elif letter == 'C' and next_letter not in ('DM'):
+                raise RomanError(f"Syntax error. {letter}{next_letter} not allowed")
+
+            result -= letter_value   
                 
-    result += inverted_roman_values[string[-1]]
+    result += symbol_to_value(string[-1])
     return result
